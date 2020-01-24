7fe7a5b4cb49b34a5e62f4daf4d1e484cf87e76a
Anthony Tarlao <anthony.tarlao@gmail.com>
2020-01-24 15:46:17 +0100

    configure project

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ const locales = [
 const defaultLocale = 'en-US';
 
 const apos = require('apostrophe')({
+  baseUrl: 'http://localhost:3000',
   shortName: 'apostrophe-boilerplate',
+  migrate: false,
   modules: {
     'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') },
     'apostrophe-i18n-static': {
@@ -30,10 +32,45 @@ const apos = require('apostrophe')({
       defaultLocale,
       locales
     },
+    // 'apostrophe-i18n-debugger': {},
     'apostrophe-workflow': {
       alias: 'workflow',
       locales: locales.map(locale => ({ label: locale.label, name: locale.value })),
-      defaultLocale
-    }
+      defaultLocale,
+      excludeTypes: [ 'apostrophe-svg-sprites' ],
+    },
+    'apostrophe-svg-sprites': {
+      maps: [
+        {
+          label: 'Cars',
+          name: 'sprite',
+          file: 'svg/4w-*.svg'
+        },
+        {
+          label: 'Places Icons',
+          name: 'places',
+          file: 'svg/places.svg'
+        },
+      ]
+    },
+    'apostrophe-svg-sprites-widgets': {},
+    'apostrophe-tiptap-rich-text-widgets': {},
+    'apostrophe-rich-text-widgets': {
+      tiptap: false
+    },
+    'apostrophe-tiptap-widgets': {
+      extend: 'apostrophe-rich-text-widgets',
+      tiptap: true,
+      label: 'Tiptap Rich Text'
+    },
+    // 'apostrophe-users': {
+    //   disableInactiveAccounts: {
+    //     neverDisabledGroups: ['admin'],
+    //     inactivityDuration: 0
+    //   }
+    // },
+    // 'apostrophe-login': {
+    //   totp: true
+    // },
   }
 });
